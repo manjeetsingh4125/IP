c2099ebea97d8abb357f83feff05fb8164e37e5d
manjeetsingh4125 <manjeetsingh4125@gmail.com>
2023-12-21 14:14:38 +0530

    first commit

---
+++ ak.java
@@ -0,0 +1,10 @@
+public class SumOfNumbers1
+{
+    public static void main(String args[]){
+        int n1 = 225,
+            n2 = 222,
+            sum;
+            sum = n1 + n2;
+            System.out.println("sum of numbers ar: "+sum);
+    }
+}
